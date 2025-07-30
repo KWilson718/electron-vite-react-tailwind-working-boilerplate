02f7e0f43f0c2f76dc2397285b1dea491ebbc22b
KWilson718 <wilsonk18@spu.edu>
2025-07-29 22:31:58 -0700

    switching to a better routing system for electron work

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -31,7 +31,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Routes,
     Route,
 } from 'react-router-dom';
